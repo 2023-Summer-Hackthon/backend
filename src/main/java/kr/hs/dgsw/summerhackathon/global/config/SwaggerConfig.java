14d1d3cd91134aa2d16ad0621f10013deb405271
pea06 <101560095+pea06@users.noreply.github.com>
2023-07-12 14:27:37 +0900

    swagger config

---
+++ src/main/java/kr/hs/dgsw/summerhackathon/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.summerhackathon.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Welcome - SummerHackathon")
+                .version("version 1.0")
+                .description("Welcome - SummerHackathon API Docs");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
