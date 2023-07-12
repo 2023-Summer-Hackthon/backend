799718b8d88d06573f8cc1e7c7cb6a387d3b917f
pea06 <101560095+pea06@users.noreply.github.com>
2023-07-12 19:49:13 +0900

    엄..

--- src/main/java/kr/hs/dgsw/summerhackathon/domain/user/presentation/UserController.java
+++ src/main/java/kr/hs/dgsw/summerhackathon/domain/user/presentation/UserController.java
@@ -2,10 +2,12 @@ package kr.hs.dgsw.summerhackathon.domain.user.presentation;
 
 import kr.hs.dgsw.summerhackathon.domain.user.presentation.dto.request.CreateUserRequest;
 import kr.hs.dgsw.summerhackathon.domain.user.presentation.dto.request.LoginUserRequest;
+import kr.hs.dgsw.summerhackathon.domain.user.presentation.dto.response.TokenDto;
 import kr.hs.dgsw.summerhackathon.domain.user.service.CommandUserService;
 import kr.hs.dgsw.summerhackathon.domain.user.service.LoginUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +21,16 @@ public class UserController {
     private final LoginUserService loginUserService;
 
     @PostMapping("/create")
-    public void createUser(CreateUserRequest request) {
+    public void createUser(
+            @RequestBody CreateUserRequest request
+    ) {
         commandUserService.createUser(request);
     }
 
     @PostMapping("/login")
-    public void loginUser(LoginUserRequest request) {
-        loginUserService.execute(request);
+    public TokenDto loginUser(
+            @RequestBody LoginUserRequest request
+    ) {
+        return loginUserService.execute(request);
     }
 }
